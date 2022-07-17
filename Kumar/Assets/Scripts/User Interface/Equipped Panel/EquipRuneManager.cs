01f0feeb38f4589fe5714ad2b49e0c306b7f5b27
Rui0517 <jiangru@sheridancollege.ca>
2022-07-17 16:15:42 -0400

    UI Typography

---
+++ Kumar/Assets/Scripts/User Interface/Equipped Panel/EquipRuneManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipRuneManager : MonoBehaviour
+{
+    [Header("Props.")]
+    [SerializeField] List<GameObject> runes = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+}
